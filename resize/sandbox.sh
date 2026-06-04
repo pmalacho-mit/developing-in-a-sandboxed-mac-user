f3b22a461aa2a056b8eb830cfd9e326d786ce60d
Parker M <95306112+pmalacho-mit@users.noreply.github.com>
2026-06-04 09:56:16 -0700

    Optimize window resizing script for efficiency

--- resize/sandbox.sh
+++ resize/sandbox.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 IN=/Users/Shared/ss_size.txt
 CHROME=28
+last=""
 while true; do
-  if [ -f "$IN" ]; then
-    size=$(cat "$IN")
-    w=$(echo "$size" | cut -d, -f1 | tr -d ' ')
-    h=$(echo "$size" | cut -d, -f2 | tr -d ' '); h=$((h - CHROME))
-    osascript -e "tell application \"System Events\" to tell process \"Code\"
-      repeat with win in windows
-        try
-          if (value of attribute \"AXMinimized\" of win) is false then
-            set position of win to {0, 0}
-            set size of win to {$w, $h}
-          end if
-        end try
-      end repeat
-    end tell" 2>/dev/null
+  IFS= read -r size < "$IN" 2>/dev/null
+  if [ -n "$size" ] && [ "$size" != "$last" ]; then
+    w=${size%%,*}; w=${w// /}
+    h=${size##*,}; h=${h// /}
+    if [[ "$w" =~ ^[0-9]+$ && "$h" =~ ^[0-9]+$ ]]; then
+      last="$size"
+      h=$((h - CHROME))
+      osascript -e "tell application \"System Events\" to tell process \"Code\"
+        repeat with win in windows
+          try
+            if (value of attribute \"AXMinimized\" of win) is false then
+              set position of win to {0, 0}
+              set size of win to {$w, $h}
+            end if
+          end try
+        end repeat
+      end tell" 2>/dev/null
+    fi
   fi
-  sleep 0.25
+  sleep 0.05
 done
