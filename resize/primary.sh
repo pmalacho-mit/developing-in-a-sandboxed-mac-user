da88855b45c4f00a6625d5714530de3fbb977eb6
Parker M <95306112+pmalacho-mit@users.noreply.github.com>
2026-06-04 10:01:00 -0700

    Refactor size logging and adjust sleep duration
    Change output method to use temporary file for size logging and reduce sleep interval.

--- resize/primary.sh
+++ resize/primary.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 OUT=/Users/Shared/ss_size.txt
+TMP="$OUT.tmp"
 last=""
 while true; do
   size=$(osascript -e 'tell application "System Events" to tell process "Screen Sharing" to get size of window 1' 2>/dev/null)
   if [ -n "$size" ] && [ "$size" != "$last" ]; then
-    echo "$size" > "$OUT"; last="$size"
+    printf '%s\n' "$size" > "$TMP" && mv "$TMP" "$OUT"
+    last="$size"
   fi
-  sleep 0.25
+  sleep 0.1
 done
